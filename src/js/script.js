cf43a3ddb987f6e3b3acbb98599a2fc88abd6ca4
Guilherme de Paula <guilhermekuskowski0@gmail.com>
2025-04-14 11:08:22 -0300

    implementando tipos de vinhos

--- src/js/script.js
+++ src/js/script.js
@@ -8,3 +8,8 @@ var nomeVinho = prompt("Por favor, informe o nome do vinho:");
 while (!nomeVinho) {
   nomeVinho = prompt("Campo obrigatório! Informe o nome do vinho:");
 }
+// Tipo do vinho
+var tipoVinho = prompt("Informe o tipo do vinho (Tinto, Branco ou Rosé):");
+while (!tipoVinho) {
+  tipoVinho = prompt("Campo obrigatório! Informe o tipo do vinho (Tinto, Branco ou Rosé):");
+}
